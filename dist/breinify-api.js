30792345fa578e37aadf0e6b1c270285b256b760
Philipp Meisen <philipp@meisen.net>
2016-07-15 16:53:09 -0700

    - new version start...

--- dist/breinify-api.js
+++ dist/breinify-api.js
@@ -1,6 +1,6 @@
 /*
  * breinify-api
- * v1.0.5
+ * v1.0.6
  **/
 /*
  * We inject a dependencyScope variable, which will be used
@@ -12373,7 +12373,7 @@ dependencyScope.jQuery = $;;
     });
 
     var BreinifyConfig = function (config) {
-        this.version = '1.0.5';
+        this.version = '1.0.6';
 
         /*
          * Validate the passed config-parameters.
@@ -12539,7 +12539,7 @@ dependencyScope.jQuery = $;;
 
     var BreinifyUser = function (user, onReady) {
         var instance = this;
-        instance.version = '1.0.5';
+        instance.version = '1.0.6';
 
         // set the values provided
         instance.setAll(user);
@@ -12797,7 +12797,7 @@ dependencyScope.jQuery = $;;
      * The one and only instance of the library.
      */
     var Breinify = {
-        version: '1.0.5',
+        version: '1.0.6',
         jQueryVersion: $.fn.jquery
     };
 
