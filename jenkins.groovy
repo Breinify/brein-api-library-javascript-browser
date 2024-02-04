6c96b97d152e7ca88e840a887156888fed3cc706
Philipp Meisen <2893215+pmeisen@users.noreply.github.com>
2024-02-04 15:56:38 -0800

    added dependencies

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,4 +1,7 @@
 def execute(String moduleDir, Object master) {
+    // add bower and grunt to the path, it's needed
+    env.PATH = "/insts/bower/versions/1.8.14/bin:/insts/grunt/versions/1.2.0/bin:${env.PATH}"
+
     master.defaultNodePipeline(moduleDir)
 }
 
