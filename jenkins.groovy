78f1b9c965d950d0c5ac63757d8ff54dea390d0f
Philipp Meisen <pmeisen@users.noreply.github.com>
2024-10-18 00:20:18 -0700

    trying without jenkins change

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,4 +1,8 @@
 def execute(String moduleDir, Object master) {
+    // if needed (currently via build script):
+    // add bower and grunt to the path, it's needed
+    // env.PATH = "/insts/bower/versions/1.8.14/bin:/insts/grunt/versions/1.2.0/bin:${env.PATH}"
+
     // seems to be an openSSL issue with phatomJs
     // see https://stackoverflow.com/questions/73004195/phantomjs-wont-install-autoconfiguration-error
     env.OPENSSL_CONF="/dev/null"
