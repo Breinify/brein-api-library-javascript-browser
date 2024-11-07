00d50b55815e4a72f3ffef12d1829203bf4d759b
Philipp Meisen <pmeisen@users.noreply.github.com>
2024-11-06 16:06:55 -0800

    uuid added

--- dist/breinify-recommendations.js
+++ dist/breinify-recommendations.js
@@ -189,7 +189,7 @@
             const replacements = {
                 _counter: 0
             };
-            const regex = /%%([a-zA-Z][a-zA-Z0-9_-]*)%%/;
+            const regex = /%%([a-zA-Z][a-zA-Z0-9_-]*(?:::[a-zA-Z][a-zA-Z0-9_-]*)?)%%/;
             const result = value.replace(regex, function (match, name) {
                 let placeholderOption = option.placeholders[name];
                 let hasPlaceholderOption = $.isFunction(placeholderOption) || typeof placeholderOption === 'string';
