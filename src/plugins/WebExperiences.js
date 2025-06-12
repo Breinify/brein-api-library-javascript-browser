2620538af5837727d7a0df035c79f6a649c886c7
Philipp Meisen <pmeisen@users.noreply.github.com>
2025-06-11 19:43:02 -0700

    added some web-experience code

--- src/plugins/WebExperiences.js
+++ src/plugins/WebExperiences.js
@@ -11,7 +11,7 @@
 
     const $ = Breinify.UTL._jquery();
     const _private = {
-        setup: function(configuration, module) {
+        setup: function (configuration, module) {
             console.log('using configuration', configuration);
 
             // ensure that we utilize the onChange method over the ready method
@@ -20,14 +20,60 @@
                 module.ready = null;
             }
 
-            // next check if we have limitations on the path and set up the isValidPage
+            // set up the logic if it's defined
+            if ($.isPlainObject(configuration.activationLogic)) {
+                this.setupActivityLogic(configuration.activationLogic, module);
+            }
+        },
+
+        setupActivityLogic: function (logic, module) {
+            const _self = this;
+
             let currentIsValidPage = null;
             if ($.isFunction(module.isValidPage)) {
                 currentIsValidPage = module.isValidPage;
             }
 
-            module.isValidPage = null;
-            module.findRequirements = null;
+            /**
+             * Checks if the current page (pathname) is valid for this module. If the method returns `true`, the
+             * `findRequirements` and `onChange` method may follow up.
+             *
+             * @return {boolean} returns `true` if the page is valid for this module, or `false` if no further execution should be handled
+             */
+            module.isValidPage = function () {
+                return _self.checkActivityLogic(logic) === true &&
+                    (currentIsValidPage === null || currentIsValidPage.call(module) === true);
+            };
+        },
+
+        checkActivityLogic: function (logic) {
+            const paths = $.isArray(logic.paths) ? logic.paths : [];
+            for (let i = 0; i < paths.length; i++) {
+                const path = $.isPlainObject(paths[i]) ? paths[i] : {};
+                const type = Breinify.UTL.isNonEmptyString(path.type);
+
+                if (type === 'ALL_PATHS') {
+                    return true;
+                }
+
+                const value = Breinify.UTL.isNonEmptyString(path.value);
+                if (value === null) {
+                    console.warn('found invalid value that was not or an empty string');
+                } else if (type === 'STATIC_PATHS') {
+                    if (value === window.location.pathname) {
+                        return true;
+                    }
+                } else if (type === 'REGEX') {
+                    if (new RegExp(type.value).test(window.location.pathname) === true) {
+                        return true;
+                    }
+                } else {
+                    console.warn('found undefined path type "' + path + '" in the activation logic, skipping');
+                }
+            }
+
+            // add a snippet usage here
+            return false;
         }
     };
 
@@ -40,18 +86,24 @@
                 return;
             }
 
-            // check if we have the trigger plugin
-            if (Breinify.plugins._isAdded('trigger') === true) {
+            if (Breinify.plugins._isAdded('trigger') !== true) {
 
-                // if we have the trigger plugin ensure that we initialize it before we set up the module
-                Breinify.plugins.trigger.init();
-                _private.setup(configuration, module);
-            } else {
+                // if we don't have the trigger plugin stop now, it's a required dependency for web-experiences
                 console.error('the trigger plugin is not available, ' +
                     'skipping setup of the web-experiences with id "' + id + '"');
                 return;
+            } else if (Breinify.plugins.api.isModule(id) === true) {
+
+                // if we have this module already there is nothing more to do
+                return;
+            } else {
+
+                // initialize the plugin (can be called doubled)
+                Breinify.plugins.trigger.init();
             }
 
+            // set up the module and add it as such
+            _private.setup(configuration, module);
             Breinify.plugins.api.addModule(id, module);
         }
     };
