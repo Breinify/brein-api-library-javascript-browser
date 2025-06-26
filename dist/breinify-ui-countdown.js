62f92874e6ad9a5a53234f1d4d34af95f5f008a8
Philipp Meisen <pmeisen@users.noreply.github.com>
2025-06-25 19:50:10 -0700

    changed update routine

--- dist/breinify-ui-countdown.js
+++ dist/breinify-ui-countdown.js
@@ -17,7 +17,7 @@
         '.countdown-banner { background-color: var(--color-background); color: var(--color-foreground); text-align: center; padding: 10px 0; }' +
         '.countdown-title { font-size: calc(var(--unit-height) * 0.25); letter-spacing: 1px; margin-bottom: 5px; text-transform: uppercase }' +
         '.countdown-timer { display: flex; justify-content: center; align-items: stretch; gap: 12px; height: var(--unit-height); }' +
-        '.time-block { flex: 0 0 auto; aspect-ratio: 1.5 / 1; display: flex; flex-direction: column; justify-content: center; align-items: center; background: transparent; }' +
+        '.time-block { position: relative; flex: 0 0 auto; aspect-ratio: 1.5 / 1; display: flex; flex-direction: column; justify-content: center; align-items: center; background: transparent; }' +
         '.time-value { font-size: calc(var(--unit-height) * 0.6); font-weight: bold; line-height: 1; }' +
         '.time-label { font-size: calc(var(--unit-height) * 0.18); margin-top: 3px; text-transform: uppercase; }' +
         '.separator { width: 1px; background-color: rgba(255, 255, 255, 0.3); height: 70%; align-self: center; }' +
